da88aa2405542733d7b5a4fe6642493f9c5f3980
Eshan Mehta <eshanmehta3125@gmail.com>
2026-05-05 00:05:34 +0530

    chore: secure firebase config using env variables

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,12 +6,12 @@ import { Platform } from 'react-native';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAcZXYKmYNxYeyDznEvqpDu34aYf44w4-U",
-  authDomain: "spendwise-e8edc.firebaseapp.com",
-  projectId: "spendwise-e8edc",
-  storageBucket: "spendwise-e8edc.firebasestorage.app",
-  messagingSenderId: "681356287684",
-  appId: "1:681356287684:web:182e40b4604bef2be1b391"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
